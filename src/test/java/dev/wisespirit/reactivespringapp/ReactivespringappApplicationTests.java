c591b3550bfe24050972af7e618c34d13ca77a85
wisespirit <@Jaxongirshoh>
2024-12-11 19:26:33 +0500

    start

---
+++ src/test/java/dev/wisespirit/reactivespringapp/ReactivespringappApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.wisespirit.reactivespringapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactivespringappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
